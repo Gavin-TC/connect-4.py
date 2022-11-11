7a0cf757b93fb527f5b47c44ff3f3ec1273a4746
Gavin <59036563+Gavin-TC@users.noreply.github.com>
2022-11-11 15:11:30 -0500

    123

--- main.py
+++ main.py
@@ -2,20 +2,62 @@ import random
 import os
 
 board = [
-    ".......",
-    ".......",
-    ".......",
-    ".......",
-    ".......",
-    "......."
+    "-------",
+    "-------",
+    "-------",
+    "-------",
+    "-------",
+    "-------"
 ]
+
+player_piece = ""
+ai_piece = ""
+current_turn = ""
+
+game_running = True
+
+
+def main():
+    pick_pieces()
+
+    while game_running:
+        draw_board()
+        os.system("PAUSE")
+
+        if current_turn == player_piece:
+            pass
+        if current_turn == ai_piece:
+            ai_plays()
+
+def ai_plays():
+    pass
 
 def drop_piece(spot, piece):
     #todo - drop a PIECE at the SPOT
     pass
 
+def check_win():
+    global board
+
+def pick_pieces():
+    global player_piece
+    global ai_piece
+
+    choices = ["X", "O"]
+
+    choice_1 = random.choice(choices)
+    choices.remove(choice_1)
+    choice_2 = choices[0]
+
+    player_piece = choice_1
+    ai_piece = choice_2
+
+
 def draw_board():
+    os.system("CLS")
     for x in range(len(board)):
         print("|" + board[x] + "|")
+    print(" 1234567 ")
 
-draw_board()
+main()
+#draw_board()
