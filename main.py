5a484784a2d7726223d63a5f0158cca5049cbe44
Gavin <59036563+Gavin-TC@users.noreply.github.com>
2022-11-11 18:03:33 -0500

    update

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import random
 import os
+import random
+import time
 
-board = [
-    "-------",
+board = [ #y, x
+    "1234567",
     "-------",
     "-------",
     "-------",
     "-------",
     "-------"
 ]
+
+# board = [
+#     "-", "-", "-", "-", "-", "-", "-",
+#     "-", "-", "-", "-", "-", "-", "-",
+#     "-", "-", "-", "-", "-", "-", "-",
+#     "-", "-", "-", "-", "-", "-", "-",
+#     "-", "-", "-", "-", "-", "-", "-",
+#     "-", "-", "-", "-", "-", "-", "-"
+# ]
 
 player_piece = ""
 ai_piece = ""
@@ -17,16 +27,14 @@ current_turn = ""
 game_running = True
 can_drop = True
 
-
 def main():
     pick_pieces()
 
     while game_running:
         draw_board()
-        os.system("PAUSE")
 
         if current_turn == player_piece:
-            spot = input("1-7: ")
+            spot = int(input("1-7: "))
             drop_piece(spot, player_piece)
         if current_turn == ai_piece:
             ai_plays()
@@ -38,14 +46,15 @@ def drop_piece(spot, piece):
     #todo - whilst dropping, check if piece below spot is empty (i.e., no pieces below)
     os.system("CLS")
 
-    while spot < 1 or spot > 7:
-        os.system("CLS)")
-        spot = input("Invalid location. 1-7: ")
-    else: pass
+    # while spot < 1 or spot > 7:
+    #     os.system("CLS)")
+    #     spot = int(input("Invalid location. 1-7: "))
+    # else: pass
 
     spot -= 1
-    if grid[spot][0] == "-":
-        print("true")
+
+    print(board[0][spot])
+    os.system("PAUSE")
 
 def check_win():
     global board
@@ -53,6 +62,7 @@ def check_win():
 def pick_pieces():
     global player_piece
     global ai_piece
+    global current_turn
 
     choices = ["X", "O"]
 
@@ -62,6 +72,11 @@ def pick_pieces():
 
     player_piece = choice_1
     ai_piece = choice_2
+
+    current_turn = player_piece
+
+    #turn_choice = [player_piece, ai_piece]
+    #current_turn = random.choice(turn_choice) need this for random turn
 
 
 def draw_board():
